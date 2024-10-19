697dd66b6759785346e6af2c816c1f692b1fbd4e
Zaptas Technologies Pvt Ltd <90035005+zaptas-technologies@users.noreply.github.com>
2024-10-19 16:10:40 +0530

    re

--- frontend/src/service/Services.jsx
+++ frontend/src/service/Services.jsx
@@ -5,10 +5,10 @@ import qs from 'qs';
 const superagent = superagentPromise(_superagent, global.Promise);
 
 //local
-const API_ROOT = '/v1/v1/api';
+// const API_ROOT = '/v1/api';
 
 //live
-// const API_ROOT = 'https://invoice.zaptas.in/v1/v1/api';
+ const API_ROOT = 'https://invoiceworld.zaptas.in/v1/api';
 
 
 const encode = encodeURIComponent;
