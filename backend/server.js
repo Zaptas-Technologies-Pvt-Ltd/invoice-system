bbafc06f72cf26395731aa6c94146ce79a2985c3
Zaptas Technologies Pvt Ltd <90035005+zaptas-technologies@users.noreply.github.com>
2024-10-19 10:33:44 +0530

    update3

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const connectDB = require('./server/database/connection');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
@@ -21,8 +22,7 @@ app.use(cors())
 // mongodb connection
 
 
-// set view engine
-app.set("view engine", "ejs")
+
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
@@ -32,7 +32,7 @@ app.use('/', require('./server/routes/router'))
 app.get('/api/hello', (req, res) => {
     res.send({ message: "Hello from backend!" });
 });
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 
 app.listen(PORT, async ()=> { console.log(`Server is running on http://localhost:${PORT}`,process.env.MONGO_URI)
 await connectDB();
